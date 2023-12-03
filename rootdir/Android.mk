c5e0e89a8fa77af3447698df157b167964afb68b
redgroul <redgroulman@gmail.com>
2023-12-03 16:41:17 +0300

    fix holi.perf

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -105,7 +105,7 @@ LOCAL_SRC_FILES    := etc/init.batterysecret.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
 include $(BUILD_PREBUILT)
 
-LOCAL_MODULE       := iinit.holi.perf.rc
+LOCAL_MODULE       := init.holi.perf.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.holi.perf.rc
